de2e855e35ddaea1d0786fb2361a7e80577a07d1
willkalb <62022821+willkalb@users.noreply.github.com>
2024-12-12 07:15:16 -0500

    9.1 updates
    nothing was wrong with the code.  copy/paste error from input

--- day9.py
+++ day9.py
@@ -1,30 +1,19 @@
 with open("./Inputs/day9.txt", "r") as f:
-    disk_map = list(map(int, f.read()))
+    disk_map = list(map(int, f.read().strip()))
 
 file_blocks = []
-for i, num in enumerate(disk_map):
-    id = i / 2
-    if id != int(id):
-        id = -1
+for i, length in enumerate(disk_map):
+    id = i // 2 if i % 2 == 0 else -1
 
-    file_blocks.extend([ int(id) for _ in range(num) ])
+    file_blocks.extend([ int(id) for _ in range(length) ])
 
-free_space_blocks = file_blocks.count(-1)
-while True:
-    while file_blocks[-1:][0] == -1:
+while -1 in file_blocks:
+    if file_blocks[-1] == -1:
         del file_blocks[-1]
-        free_space_blocks -= 1
+        continue
 
-    if free_space_blocks == 0:
-        break
-
-    file_blocks[next(i for i, id in enumerate(file_blocks) if id == -1)] = file_blocks[-1:][0]
+    file_blocks[file_blocks.index(-1)] = file_blocks[-1]
     del file_blocks[-1]
-    free_space_blocks -= 1
 
-# file_blocks = file_blocks + ([-1] * free_space_blocks)
-
-print(f"Answer 1: { sum([ i * id for i, id in enumerate(file_blocks) ]) }")
-# 5001859401903 too low
-# 6412389511995 too high
+print(f"Answer 1: { sum( i * id for i, id in enumerate(file_blocks) ) }")
 # print(f"Answer 2: { len(set(map(lambda x: (x[0], x[1]), resonate_antinodes))) }")
