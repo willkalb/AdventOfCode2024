01c83f931178915f07017c3879dee13e4a0c82b2
willkalb <62022821+willkalb@users.noreply.github.com>
2024-12-19 22:01:31 -0500

    refactor day19

--- Days/day19.py
+++ Days/day19.py
@@ -3,42 +3,19 @@ with open("./Inputs/day19.txt", "r") as f:
     towels = lines[0].replace(" ", "").split(",")
     patterns = [ p for p in lines[2:]]
 
-valid_pattern_cache = dict()
-def isValid(iv_pattern):
-    if iv_pattern in valid_pattern_cache:
-        return valid_pattern_cache[iv_pattern]
-
-    if iv_pattern in towels:
-        valid_pattern_cache[iv_pattern] = True
-        return True
-    
-    valid = False
-    for t in towels:
-        if valid == True:
-            valid_pattern_cache[iv_pattern] = True
-            return True
-        if t == iv_pattern[:len(t)]:
-            valid = isValid(iv_pattern[len(t):])
-        
-    
-    valid_pattern_cache[iv_pattern] = False
-    return False
-
 arrangement_cache = dict()
-def getPossibleArrangementsCount(gpa_pattern, depth = 0):
+def getPossibleArrangementsCount(gpa_pattern):
     if gpa_pattern in arrangement_cache:
         return arrangement_cache[gpa_pattern]
+    
+    if len(gpa_pattern) == 0: 
+        return 1
     
     arrangements = 0
     for t in towels:
         if t == gpa_pattern[:len(t)]:
-            p_as = getPossibleArrangementsCount(gpa_pattern[len(t):], depth + 1)
-
-            if p_as == 0 and gpa_pattern[len(t):] == "":
-                arrangements += 1
-                continue
-
-            arrangements += p_as
+            arrangements += getPossibleArrangementsCount(gpa_pattern[len(t):])
+            
     arrangement_cache[gpa_pattern] = arrangements
 
     return arrangements
@@ -47,8 +24,9 @@ def getPossibleArrangementsCount(gpa_pattern, depth = 0):
 valid_pattern_count = 0
 arrangement_count = 0
 for pattern in patterns:
-    valid_pattern_count += isValid(pattern)
-    arrangement_count += getPossibleArrangementsCount(pattern)
+    poss_arr_count = getPossibleArrangementsCount(pattern)
+    valid_pattern_count += 1 if poss_arr_count > 0 else 0
+    arrangement_count += poss_arr_count
 
 print(f"Answer 1: { valid_pattern_count }")
 print(f"Answer 2: { arrangement_count }")
