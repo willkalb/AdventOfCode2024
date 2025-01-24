4586cbb1fb9354673538e0ce6b4366d3800fe85d
willkalb <62022821+willkalb@users.noreply.github.com>
2025-01-23 22:19:23 -0500

    day20 stuff. i don't remember

--- Days/day20.py
+++ Days/day20.py
@@ -1,6 +1,6 @@
 grid = []
-# (x, y, d)
-curr_pos_og = (0, 0, 0)
+# (x, y)
+curr_pos = (0, 0)
 end_pos = (0, 0)
 
 with open("./Inputs/day20.txt", "r") as f:
@@ -8,7 +8,7 @@ with open("./Inputs/day20.txt", "r") as f:
         grid.append(list(line))
         for ix, x in enumerate(line):
             if x == "S":
-                curr_pos_og = (ix, iy, 1)
+                curr_pos = (ix, iy)
                 continue
             if x == "E":
                 end_pos = (ix, iy)
@@ -18,7 +18,7 @@ dir = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 def getNeighbors(gn_grid, pos):
     n = []
     for i in range(4):
-        p_c = (pos[0] + dir[i][0], pos[1] + dir[i][1], i), 1 #if i == pos[2] else 1001
+        p_c = (pos[0] + dir[i][0], pos[1] + dir[i][1]), 1
         if gn_grid[p_c[0][1]][p_c[0][0]] == "#":
             continue
         n.append(p_c)
@@ -33,84 +33,75 @@ def buildPrevList(prevDict, curr):
 
     return p
 
-visited = { (x, y, d): False for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
-costFromStart = { (x, y, d): float("inf") for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
-prev = { (x, y, d): None for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
+def getPhasedTilePositions(tile_pos, phase_dur):
+    phased_tiles = list()
+    for p in range(1, phase_dur):
+        phased_tiles.append((tile_pos[0] - (phase_dur - 1 - p), tile_pos[1] - p))
+        phased_tiles.append((tile_pos[0] + p, tile_pos[1] + (phase_dur - 1 - p)))
+        phased_tiles.append((tile_pos[0] + (phase_dur - 1 - p), tile_pos[1] + p))
+        phased_tiles.append((tile_pos[0] - p, tile_pos[1] - (phase_dur - 1 - p)))
 
-costFromStart[curr_pos_og] = 0
+    return list(filter(lambda t: 0 < t[0] < len(grid[0]) - 1 and 0 < t[1] < len(grid) - 1, phased_tiles))
 
-queue = [(curr_pos_og, costFromStart[curr_pos_og])]
-while len(queue) > 0:
-    curr_pos, curr_cost = queue.pop(0)
+def findShortestPath(fsp_start_pos, fsp_end_pos, fsp_grid):
+    visited = { (x, y): False for y in range(len(fsp_grid)) for x in range(len(fsp_grid[y])) }
+    costFromStart = { (x, y): float("inf") for y in range(len(fsp_grid)) for x in range(len(fsp_grid[y])) }
+    prev = { (x, y): None for y in range(len(fsp_grid)) for x in range(len(fsp_grid[y])) }
 
-    if curr_pos[:2] == end_pos[:2]:
-        break
+    costFromStart[fsp_start_pos] = 0
 
-    neighbors = getNeighbors(grid, curr_pos)
+    queue = [(fsp_start_pos, costFromStart[fsp_start_pos])]
+    while len(queue) > 0:
+        curr_pos, curr_cost = queue.pop(0)
 
-    for neighbor, cost in neighbors:
-        if visited[neighbor] == True:
+        if curr_pos == fsp_end_pos:
+            break
+
+        neighbors = getNeighbors(fsp_grid, curr_pos)
+
+        for neighbor, cost in neighbors:
+            if visited[neighbor] == True:
+                continue
+            visited[neighbor] = True
+            costToNeighbor = costFromStart[curr_pos] + cost
+            if costToNeighbor > costFromStart[neighbor]:
+                continue
+            costFromStart[neighbor] = costToNeighbor
+
+            prev[neighbor] = curr_pos
+        
+            queue.append((neighbor, costToNeighbor))
+            queue.sort(key = lambda nn: nn[1])
+
+    path = buildPrevList(prev, curr_pos)
+    path.reverse()
+
+    return path
+
+path = findShortestPath(curr_pos, end_pos, grid)
+
+shorter_paths_count = 0
+
+phase_duration = 2
+for i, tile in enumerate(path):
+    if i % 500 == 0:
+        print(i)
+
+    phased_tiles = getPhasedTilePositions(tile, phase_duration)
+    og_grid_tiles = { tile: grid[tile[1]][tile[0]] for tile in phased_tiles }
+    for pt in phased_tiles:
+        if pt in path[:i]:
             continue
-        visited[neighbor] = True
-        costToNeighbor = costFromStart[curr_pos] + cost
-        if costToNeighbor > costFromStart[neighbor]:
-            continue
-        costFromStart[neighbor] = costToNeighbor
 
-        prev[neighbor] = curr_pos
-    
-        queue.append((neighbor, costToNeighbor))
-        queue.sort(key = lambda nn: nn[1])
+        grid[pt[1]][pt[0]] = "."
 
-slowest_time = costFromStart[curr_pos]
+        new_path = findShortestPath(pt, end_pos, grid)
+        new_path = path[:i + 1] + new_path
 
-times = []
-for iy in range(1, len(grid) - 1):
-    for ix in range(1, len(grid[iy]) - 1):
-        print(f"{ix}, {iy}")
-        if grid[iy][ix] in [".", "S", "E"]:
-            continue
+        if len(new_path) <= len(path) - 40:
+            shorter_paths_count += 1
 
-        og_pos_tile = grid[iy][ix]
-        grid[iy][ix] = "."
+        grid[pt[1]][pt[0]] = og_grid_tiles[pt]
 
-        visited = { (x, y, d): False for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
-        costFromStart = { (x, y, d): float("inf") for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
-        prev = { (x, y, d): None for y in range(len(grid)) for x in range(len(grid[y])) for d in range(4) }
-
-        costFromStart[curr_pos_og] = 0
-
-        queue = [(curr_pos_og, costFromStart[curr_pos_og])]
-        while len(queue) > 0:
-            curr_pos, curr_cost = queue.pop(0)
-
-            if curr_cost > slowest_time - 100:
-                break
-
-            if curr_pos[:2] == end_pos[:2]:
-                break
-
-            neighbors = getNeighbors(grid, curr_pos)
-
-            for neighbor, cost in neighbors:
-                if visited[neighbor] == True:
-                    continue
-                visited[neighbor] = True
-                costToNeighbor = costFromStart[curr_pos] + cost
-                if costToNeighbor > costFromStart[neighbor]:
-                    continue
-                costFromStart[neighbor] = costToNeighbor
-
-                prev[neighbor] = curr_pos
-            
-                queue.append((neighbor, costToNeighbor))
-                queue.sort(key = lambda nn: nn[1])
-
-        grid[iy][ix] = og_pos_tile
-
-        times.append(costFromStart[curr_pos])
-
-fast_times = { slowest_time - t: times.count(t) for t in times if slowest_time - t >= 100 }
-
-print(f"Answer 1: { sum(v for _k, v in fast_times.items()) }")
+print(f"Answer 1: { shorter_paths_count }")
 # print(f"Answer 2: { len({ tile[:2] for path in paths for tile in path }) }")
